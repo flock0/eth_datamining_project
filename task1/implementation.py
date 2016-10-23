6f46e3e714f80ba73cb20d6b343eb8786960460c
Florian Chlan <florian@chlan.net>
2016-10-23 12:58:33 +0200

    Create signature vector using min-hash

--- task1/implementation.py
+++ task1/implementation.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 HASH_PRIME = 7817 # Choose some random prime number larger than the number of videos
 RNG_SEED = 618984 # Random seed to be used by the RNG (so that each mapper derives the same hash functions)
@@ -17,10 +18,19 @@ def mapper(key, value):
     videoString = value[:VIDEO_ID_STRING_LENGTH]
     videoId = int(videoString[VIDEO_ID_STRING_PREFIX_LENGTH:])
     shingles = np.fromstring(value[VIDEO_ID_STRING_LENGTH + 1:], dtype=int, sep=' ')
-    
+    shingles.sort()
+
     # Decide on R * B hash functions
     np.random.seed(RNG_SEED)
-    hash_coefficients = np.random.randint(low=1, high=HASH_PRIME, size=(R * B, 2)) # Create random hash functions by drawing values for a and b
+    hashCoefficients = np.random.randint(low=1, high=HASH_PRIME, size=(R*B, 2)) # Create random hash functions by drawing values for a and b
+
+
+    # Create signature vector using min-hash
+    signatureVector = np.full(R*B, sys.maxint)
+    for shingle in shingles:
+        for h in range(0, R*B):
+            hashCoeffPair = hashCoefficients[h]
+            signatureVector[h] = min(signatureVector[h], hash(shingle, hashCoeffPair[0], hashCoeffPair[1]))
 
     yield 1, 1
 
