675247c62ff8abf2ed91e071fb75578faa025de0
Daniel Yu <yuda@student.ethz.ch>
2016-10-25 13:54:13 +0200

    added python documentation comments (PEP 257 standard)

--- task1/implementation.py
+++ task1/implementation.py
@@ -7,23 +7,47 @@ NUM_VIDEOS = 700
 SIMILARITY_THRESHOLD = 0.85
 R = 13
 B = 55
+
+
 def hashShingle(x, a, b):
+    """Calculate the hash for a shingle with parameter a and b
+
+    Keyword arguments:
+    x -- the shingle to hash
+    a,b -- paramenter for our hash function
+    """
     return ((a * x + b) % HASH_PRIME) % NUM_VIDEOS
 
 def hashBand(xArr, coeffArr):
+    """Calculate the hash for a band xArr with parameter coeffArr
+
+    Keyword arguments:
+    xArr -- the band to hash
+    coeffArr -- Array of parameter for our hash function
+    """
     hashSum = 0
     for i in range(0, R):
         hashSum += ((coeffArr[i, 0] * xArr[i] + coeffArr[i, 1]) % HASH_PRIME) % NUM_VIDEOS
     return hashSum % NUM_VIDEOS
 
 def jaccardSimilarity(candidate1, candidate2):
+    """Calculate the jaccardSimilarity of the two (videoId,videoShingle)-tuple candidate1 and candidate2
+
+    Keyword arguments:
+    candidate1, candidate2 -- two video tuple of (videoId, videoShingle) to calculate the jaccardSimilarity 
+    """
     return np.intersect1d(candidate1[1], candidate2[1]).size / float(np.union1d(candidate1[1], candidate2[1]).size)
 
 def mapper(key, value):
-    # key: None
-    # value: one line of input file
+    """The Mapper for the mapReduce. It first calculate the signature vector and based on that the the hashbucket
 
-    # Extract the videoID and shingles from the input line
+    Keyword arguments:
+    key -- None
+    video -- one line of input
+
+    Outputs:
+    List of ((bandId, hash bucket of the hashed band), (videoId, shingles)) tuples
+    """
     VIDEO_ID_STRING_LENGTH = 15
     VIDEO_ID_STRING_PREFIX_LENGTH = 6
     videoString = value[:VIDEO_ID_STRING_LENGTH]
@@ -53,8 +77,15 @@ def mapper(key, value):
         yield str((i, hashBand(signatureVector[i*R:(i+1)*R], bandHashCoefficients))), (videoId, shingles)
     
 def reducer(key, values):
-    # key: key from mapper used to aggregate
-    # values: list of all value for that key
+    """The Reducer for the mapReduce. It checks all the pairs of values, if the Jaccard similarity is above the threshold
+
+    Keyword arguments:
+    key -- (bandId, hash bucket of the hashed band) tuples
+    video -- (videoId, shingles)
+
+    Outputs:
+    a list of true pairs of candidate, which has a Jaccard similarity of at least the threshold
+    """
 
     for c in range(0, len(values)):
         candidate1 = values[c]
