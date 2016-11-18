a0f574c4ffac6b9a01d7ff216bb37ab5648c4817
Daniel Yu <yu_daniel@me.com>
2016-11-18 14:16:38 +0100

    ADAM implemented

--- task2/implementation_daniel.py
+++ task2/implementation_daniel.py
@@ -14,65 +14,85 @@ def parseValue(value):
     Y = matrix[:,1]
     return X,Y
 
-def caculateGradient(X, Y, lamda, w, batchsize, dimension):
-    gradient = np.zeros(dimension,1)
+def calculateGradient(X, Y, lamda, w, batchsize, dimension):
+    index = np.random.choice(X.shape[0], size = batchsize, replace=False)
+    gradient = np.zeros(dimension)
+    counter = 0
 
-    for i in range(0, batchsize):
+    for i in index:
         # Get the row
-        x = X[:,i]
+        x = X[i,:]
         y = Y[i]
         # Check if it is classified correctly with w
         # If not, add the negative direction to the gradient
-        if (y*w.T.dot(x) < 1):
-            gradient = gradient - np.multiply(y,x)
+        #print("check", y*w.dot(x))
+        if y*w.dot(x) < 1:
+            gradient = gradient - y*x
+            counter = counter + 1
+    print("Counter",counter)
 
     # Add the content to the gradient
-    gradient = gradient + np.multiply(lamda, w)
+    gradient = gradient + lamda*w
     return gradient
 
 
 
 def mapper(key, value):
-    # key: None
-    # value: one line of input file
-    # Implements the ADAM method
+    '''key: None
+    value: one line of input file
+    Implements the ADAM method (complementary to our EVA method)
+    '''
 
     # Hyper parameters
-    lamda = 1
-    alpha = 0.001
-    beta0 = 0.9
-    beta1 = 0.999
+    lamda = 10e-9
+    alpha = 1
+    beta1 = 0
+    beta2 = 1
     epsilon = 1e-8
-    T = 100
-    batchsize = 128
+    T = 1000
+    batchsize = 1024
 
-    # Parse the input
+    # Parse the input and shuffle it
     X,Y = parseValue(value)
-    print(X.shape)
-    print(Y.shape)
+    # TODO: shuffle the input
 
-    # Get the dimension
+    # Get the dimension and samplesize
     d = X.shape[1]
+    n = X.shape[0]
 
-    # Initialize m_0, v_0 to zero
-    m = np.zeros(d,1)
-    v = np.zeros(d,1)
+    # Initialize m_0, v_0, w_0 to zero
+    m = np.zeros(d)
+    v = np.zeros(d)
+    w = np.zeros(d)
 
-    # For t=1..T:
     for t in range(1,T):
         # Calculate the gradient g_t
+        gradient = calculateGradient(X,Y,lamda,w,batchsize,d)
+        #print("gradient", gradient)
         # Calculate the first moment m_t
+        m = beta1 * m + (1-beta1) * gradient
+        #print("first moment:", m)
         # Calculate the second moment v_t
+        v = beta2 * v + (1-beta2) * (gradient ** 2)
+        #print("second moment:", v)
         # Calculate the first bias correction
+        m = m / (1 - (beta1 ** t))
+        #print("first correction", m)
         # Calculate the second bias correction
+        v = v / (1 - (beta2 ** t))
+        #print("second correction:", m)
         # Update the weight vector
-
-    
-    yield "key", "value"  # This is how you yield a key, value pair
+        w = w - alpha * m / (np.sqrt(v) + epsilon)
+        #sprint("new weight vector:", w)
+    yield "key", w  # This is how you yield a key, value pair
 
 
 def reducer(key, values):
-    # key: key from mapper used to aggregate
-    # values: list of all value for that key
-    # Implements the EVA method (EVArage method, complementary to our ADAM)
-    yield np.random.randn(400)
+    '''
+    key: key from mapper used to aggregate
+    values: list of all value for that key
+    Implements the EVA method (EVArage method, complementary to our ADAM)
+    '''
+    avg = np.average(values, axis=0)
+    print(avg.shape)
+    yield avg
